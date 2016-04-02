10273af4026508635d29d72f930bb03a87bed5b1
Tatiana souza santana <Tatiana souza santana>
2016-04-02 19:01:31 -0300

    Upload with Login

--- config/general.js
+++ config/general.js
@@ -1,10 +1,10 @@
 const path = require('path');
 module.exports = {
-	'secret' : 'blackbearddemogidascruzesesappaulo',
+	'secret' : '',
     'database': process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/',
     'dbname': 'cloud',
     'root': path.join(__dirname, '..'),
-    'cloud_name' : 'djaysgwfa',
-    'api_key' : '998675877334278',
-    'api_secret': 'ObZXbU4RSFaEwTQ_8FYPNQd22IQ',
+    'cloud_name' : '',
+    'api_key' : '',
+    'api_secret': '',
 };
